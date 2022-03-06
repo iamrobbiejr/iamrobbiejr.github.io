4641583770374efa14572360cf568b0575904e1f
Robbie_Malunga <46316955+iamrobbiejr@users.noreply.github.com>
2022-03-06 20:27:58 +0200

    admin commit

---
+++ admin/src/pages/login.js
@@ -0,0 +1,114 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+
+
+const Login = () => {
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      email: 'admin@example.com',
+      password: 'Password123'
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email(
+          'Must be a valid email')
+        .max(255)
+        .required(
+          'Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required(
+          'Password is required')
+    }),
+    onSubmit: () => {
+      router.push('/customers');
+    }
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Login | Mental Health</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%'
+        }}
+      >
+        <Container maxWidth="sm">
+
+          <form onSubmit={formik.handleSubmit}>
+            <Box sx={{ my: 3 }}>
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
+                Sign in
+              </Typography>
+              <Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+                Sign in on the internal platform
+              </Typography>
+            </Box>
+
+
+            <TextField
+              error={Boolean(formik.touched.email && formik.errors.email)}
+              fullWidth
+              helperText={formik.touched.email && formik.errors.email}
+              label="Email Address"
+              margin="normal"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.email}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.password && formik.errors.password)}
+              fullWidth
+              helperText={formik.touched.password && formik.errors.password}
+              label="Password"
+              margin="normal"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="password"
+              value={formik.values.password}
+              variant="outlined"
+            />
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                disabled={formik.isSubmitting}
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Sign In Now
+              </Button>
+            </Box>
+
+          </form>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Login;
